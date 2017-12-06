dd74bafae354c3f03db4a2bd2c708c138c32baf6
Sean Loh <sean.loh@capgemini.com>
2017-12-06 11:17:14 +0800

    Validate jwt expirty

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import 'reflect-metadata';
+aimport 'reflect-metadata';
 import 'zone.js/dist/zone-node';
 import { renderModuleFactory } from '@angular/platform-server'
 import { enableProdMode } from '@angular/core'
 import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
-import { request} from 'request';
+import { request } from 'request';
 import * as environment from '../environment.json';
 
-var jwt = require ('jsonwebtoken'); 
+var jwt = require('jsonwebtoken');
+var moment = require('moment');
 
 enableProdMode();
 
@@ -25,32 +26,34 @@ import { ngExpressEngine } from '@nguniversal/express-engine';
 
 var secretKey = (<any>environment).pwd;
 /* Server-side rendering */
-  function angularRouter(req, res) {
-    
-    var token = generateToken();
-    res.render(join(DIST_FOLDER, 'browser','index.html'), {
-        req: req,
-        res: res,
-        providers: [{
-          provide: 'serverUrl',
-          useValue: `http://localhost:4200`
-        },{
-          provide: 'token',
-          useValue: token
-        }]
-      });
-    
-    }
+function angularRouter(req, res) {
 
-    function generateToken(){
-      return jwt.sign({ id: (<any>environment).userName }, secretKey, {
-        expiresIn: 60 * 60
-      });
-    }
+  var token = generateToken();
+  res.render(join(DIST_FOLDER, 'browser', 'index.html'), {
+    req: req,
+    res: res,
+    providers: [{
+      provide: 'serverUrl',
+      useValue: `http://localhost:4200`
+    }, {
+      provide: 'token',
+      useValue: token
+    }]
+  });
+
+}
+
+function generateToken() {
+  const expiryDate = moment().add(1, 'hours').valueOf();
+  // const expiryDate = moment().add(-1, 'hours').valueOf();
+  return jwt.sign({ expiryDate: expiryDate, targetAudience: '', id: (<any>environment).userName }, secretKey, {
+    expiresIn: 60 * 60
+  });
+}
 
 //app.get('/', angularRouter);
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModuleNgFactory
 }));
@@ -58,20 +61,30 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', 'src')
 
-app.get("/api/*",(req,res)=> {
-    console.log(req.headers.authorization);
-    console.log(req.headers.authorization.split(" ")[1]);
-    jwt.verify(req.headers.authorization.split(" ")[1],secretKey, function(err, decoded) {
-      console.log(decoded);
-      console.log(err);
-      if(err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-      else return res.send({"message":"success"});
+app.get("/api/*", (req, res) => {
+  console.log(req.headers.authorization);
+  console.log(req.headers.authorization.split(" ")[1]);
+  jwt.verify(req.headers.authorization.split(" ")[1], secretKey, function (err, decoded) {
+    //  console.log('decoded', decoded); console.log('err', err);
+    if (err || !decoded) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    const currentDateTime = moment();
+    const expiryDateTime = moment(decoded.expiryDate);
+    const differenceTimeMs = expiryDateTime.diff(currentDateTime);
+    const messageResponse = differenceTimeMs < 0 ? 'Please check expired token' : 'success'
+
+    console.log('currentDateTime', currentDateTime);
+    console.log('expiryDateTime', expiryDateTime);
+    console.log('differenceTimeMs', differenceTimeMs);
+    return res.send({
+      "differenceTimeMs": differenceTimeMs, "isTokenExpired": (differenceTimeMs < 0), "message": messageResponse
     });
+  });
 });
 
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
 
-app.get('*',angularRouter);
+app.get('*', angularRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}!`);
